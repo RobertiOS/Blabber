05415a17448076b4944c29928b717a00649a8212
Robert <robert.ios.developer@gmail.com>
2022-04-17 15:11:25 -0600

    add read messages capability

--- Blabber/Model/BlabberModel.swift
+++ Blabber/Model/BlabberModel.swift
@@ -1,38 +1,6 @@
-/// Copyright (c) 2021 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import Foundation
 import CoreLocation
-//import Combine
+import Combine
 import UIKit
 
 /// The app model that communicates with the server.
@@ -86,6 +54,18 @@ class BlabberModel: ObservableObject {
     var iterator = stream.lines.makeAsyncIterator()
     
     guard let first = try await iterator.next() else { throw "No response from the server" }
+    guard let data = first.data(using: .utf8),
+          let status = try? JSONDecoder().decode(ServerStatus.self, from: data)
+    else { throw "Invalid response from server" }
+    messages.append(Message(message: "\(status.activeUsers) active users"))
+    
+    for try await line in stream.lines {
+      if let data = line.data(using: .utf8),
+         let update = try? JSONDecoder().decode(Message.self, from: data) {
+        messages.append(update)
+      }
+    }
+    
   }
 
   /// Sends the user's message to the chat server
